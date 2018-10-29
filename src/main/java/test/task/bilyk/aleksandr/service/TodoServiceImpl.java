f574c92f746299d3b2a28a8af673c91d9181c380
AleksandrBLK <info@bilyk.kiev.ua>
2018-10-29 03:32:42 +0200

    web todo

--- src/main/java/test/task/bilyk/aleksandr/service/TodoServiceImpl.java
+++ src/main/java/test/task/bilyk/aleksandr/service/TodoServiceImpl.java
@@ -13,7 +13,6 @@ public class TodoServiceImpl implements BackendlessTodoService {
     private final BackendlessTodoRepository repository;
 
 
-
     public TodoServiceImpl(BackendlessTodoRepository repository) {
         this.repository = repository;
     }
@@ -21,7 +20,7 @@ public class TodoServiceImpl implements BackendlessTodoService {
 
     @Override
     public void delete(Todo todo) throws UnirestException {
-        if (repository.findById(todo.getObjectId())){
+        if (repository.findById(todo.getObjectId())) {
             repository.delete(todo);
         }
 
@@ -34,7 +33,7 @@ public class TodoServiceImpl implements BackendlessTodoService {
 
     @Override
     public void update(Todo todo, String objectId) throws UnirestException {
-        if (repository.findById(objectId)){
+        if (repository.findById(objectId)) {
             todo.setObjectId(objectId);
             repository.save(todo);
         }
@@ -52,13 +51,16 @@ public class TodoServiceImpl implements BackendlessTodoService {
 
     @Override
     public Todo findByIdAndGet(String ObjectId) throws UnirestException {
-        return repository.findByIdAndGet(ObjectId);
+        if (repository.findById(ObjectId)) {
+            return repository.findByIdAndGet(ObjectId);
+        }
+        return null;
     }
 
     @Override
-    public void deletingIfCompleted (int minutes) {
+    public void deletingIfCompleted(int minutes) {
         TimerTask timerTask = new TimerDeletingTask();
         Timer timer = new Timer(true);
-        timer.scheduleAtFixedRate(timerTask,  minutes * 60000,minutes * 60000);
+        timer.scheduleAtFixedRate(timerTask, minutes * 60000, minutes * 60000);
     }
 }
